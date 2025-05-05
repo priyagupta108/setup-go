dfbe64c7a394a829d0ef4d504bf257f90afd88d5
Priyagupta108 <priyagupta108@github.com>
2025-05-05 11:07:45 +0530

    TEST

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -93481,7 +93481,7 @@ function getManifest(auth) {
             return yield getManifestFromRepo(auth);
         }
         catch (err) {
-            core.info('Test log to identify rate limit failure');
+            core.warning('Test log to identify rate limit failure');
             core.debug('Fetching the manifest via the API failed.');
             if (err instanceof Error) {
                 core.debug(err.message);
